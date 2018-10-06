0ac199692ce0f4c49f073074d3d06fac235bfbd4
Gustav Karlström <gustav.karlstrom96@hotmail.com>
2018-10-06 22:17:14 +0200

    Validated all requests, basic functionality complete

--- routes/api.js
+++ routes/api.js
@@ -53,59 +53,85 @@ router.post('/authenticate', function(req, res) {
     });
 });
 
-// ----- QuizRoom ROOM ROUTES (get, add, change, delete)-----
+// ----- QuizRoom ROOM ROUTES (get, add, edit, delete)-----
 router.get('/quiz-rooms', (req, res) => {
-    let response = {
-        type: 'GET',
-    };
-    res.send(response);
+    QuizRoom.find({}).then((result) => {
+        res.send(result);
+    })
 });
 
-router.post('/quiz-rooms/add', (req, res, next) => {
-    QuizRoom.create(req.body).then((room) => {
-        res.send(room);
-    }).catch(next);
-});
-router.put('/quiz-rooms/change/:id', (req, res, next) => {
-    QuizRoom.findByIdAndUpdate({_id: req.params.id}, req.body).then(() => {
-        Quiz.findOne({_id: req.params.id}).then((room) => {
-            res.send(room);
-        })
+router.post('/quiz-rooms/add', passport.authenticate('jwt', { session: false}), (req, res, next) => {
+    authCheck(req, (passed) => {
+        if (passed) {
+            QuizRoom.create(req.body).then((room) => {
+                res.send({
+                    added: true,
+                    room
+                });
+            }).catch(next);
+        } else {
+            res.status(403).send({success: false, msg: 'Unauthorized'});
+        }
     });
 });
+router.put('/quiz-rooms/edit/:id', passport.authenticate('jwt', { session: false}), (req, res, next) => {
+    authCheck(req, (passed) => {
+        if (passed) {
+            QuizRoom.findByIdAndUpdate({_id: req.params.id}, req.body, {new: true}).then((updatedDoc) => {
+               res.send(updatedDoc);
+            });
+        } else {
+            res.status(403).send({success: false, msg: 'Unauthorized'});
+        }
+    })
 
-
-router.delete('/quiz-rooms/delete/:id', (req, res, next) => {
-    QuizRoom.findByIdAndRemove({_id: req.params.id}).then((room) => {
-        res.send(room);
-    });
 });
 
 
+router.delete('/quiz-rooms/delete/:id', passport.authenticate('jwt', { session: false}), (req, res, next) => {
+    authCheck(req, (passed) => {
+        if (passed) {
+            QuizRoom.findByIdAndRemove({_id: req.params.id}).then((room) => {
+                res.send({
+                    deleted: true,
+                    room
+                });
+            });
+        } else {
+            res.status(403).send({success: false, msg: 'Unauthorized'});
+        }
+    });
+});
 
 router.get('/memberinfo', passport.authenticate('jwt', { session: false}), function(req, res) {
+    authCheck(req, (what) => {
+        if (what) {
+         res.json({success:true});
+        } else {
+            res.status(403).send({success: false, msg: 'Unauthorized'});
+        }
+    });
+});
+
+const authCheck = (req, callback) => {
     const token = getToken(req.headers);
     if (token) {
         const decoded = jwt.decode(token, secret);
         User.findOne({
             name: decoded.name
-        }, function(err, user) {
-            if (err) throw err;
-
-            if (!user) {
-                return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
-            } else {
-                res.json({success: true, msg: 'Welcome in the member area ' + user.name + '!'});
+        }, (err, user) => {
+            if (!err && user) {
+                callback(true);
             }
         });
     } else {
-        return res.status(403).send({success: false, msg: 'No token provided.'});
+        return false
     }
-});
+}
 
-getToken = function (headers) {
+const getToken = (headers) => {
     if (headers && headers.authorization) {
-        var parted = headers.authorization.split(' ');
+        const parted = headers.authorization.split(' ');
         if (parted.length === 2) {
             return parted[1];
         } else {
