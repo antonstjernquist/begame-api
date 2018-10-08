fdc6bee67de54b6cad70dec4213809dcb41f7ea0
Gustav Karlström <gustav.karlstrom96@hotmail.com>
2018-10-08 19:46:44 +0200

    mmongodb

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 
 module.exports = {
     'secret': 'coolKid420',
-    'database': 'mongodb://localhost/begame'
+    'database': 'mongodb://david-admin:E*3uXRfBP5Uz@ds125713.mlab.com:25713/begame-api'
 };
