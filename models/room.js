d8250607a93d5ecab51f9cc3ea06d771a033134a
Johan Augustsson <j.augustsson80@gmail.com>
2018-10-13 20:16:10 +0200

    fix timestamp and createdBy and openForAnswer

--- models/room.js
+++ models/room.js
@@ -11,14 +11,24 @@ const RoomSchema = new Schema({
         type: String,
         required: [true, 'Name field is required'],
     },
+    createdBy: {
+        type: String,
+        required: [true, 'Name field is required'],
+    },
     currentQuestion: Number,
     active: {
         type: Boolean,
         required: [true, 'flag is required'],
     },
+    openForAnswer: {
+        type: Boolean,
+        required: [true, 'flag is required'],
+    }
     quiz: Object,
 
-});
+},
+  {timestamps: true}
+);
 
 const Room = mongoose.model('Room', RoomSchema);
 
